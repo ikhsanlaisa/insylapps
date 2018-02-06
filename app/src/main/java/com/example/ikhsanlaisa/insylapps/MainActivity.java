ee4c660e0a125d18dc62d52290cd48f27f408cc3
ikhsan laisa <ikhsanlaisa96@gmail.com>
2018-02-06 19:23:28 +0700

    fb success

--- app/src/main/java/com/example/ikhsanlaisa/insylapps/MainActivity.java
+++ app/src/main/java/com/example/ikhsanlaisa/insylapps/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.ikhsanlaisa.insylapps;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,11 +28,17 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private CallbackManager mCallbackManager;
     private static final String TAG = "FACELOG";
     private FirebaseAuth mAuth;
+    private Button mBtnfb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,30 +48,38 @@ public class MainActivity extends AppCompatActivity {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
-        // Initialize Facebook Login button
+        mBtnfb = findViewById(R.id.login_button1);
+
         mCallbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email", "public_profile");
-        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                handleFacebookAccessToken(loginResult.getAccessToken());
-            }
 
-            @Override
-            public void onCancel() {
-                Log.d(TAG, "facebook:onCancel");
-                // ...
-            }
 
+        mBtnfb.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onError(FacebookException error) {
-                Log.d(TAG, "facebook:onError", error);
-                // ...
+            public void onClick(View v) {
+
+                mBtnfb.setEnabled(false);
+                List < String > permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "public_profile");
+                LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, permissionNeeds);
+                LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+                            @Override
+                            public void onSuccess(LoginResult loginResult) {
+                                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                                handleFacebookAccessToken(loginResult.getAccessToken());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                Log.d(TAG, "facebook:onCancel");
+                            }
+
+                            @Override
+                            public void onError(FacebookException error) {
+                                Log.d(TAG, "facebook:onError", error);
+                            }
+                        }
+                );
             }
         });
-
     }
 
     @Override
@@ -102,16 +123,16 @@ public class MainActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            mBtnfb.setEnabled(true);
                             updateUI();
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Log.d(TAG, "signInWithCredential:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
+                            mBtnfb.setEnabled(true);
                             updateUI();
                         }
-
-                        // ...
                     }
                 });
     }
